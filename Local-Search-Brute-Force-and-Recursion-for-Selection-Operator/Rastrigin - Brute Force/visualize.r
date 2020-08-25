c1693ed2da3d392cbe6a995e81308781fbc37ec4
Todor Balabanov <todor.balabanov@gmail.com>
2020-08-25 18:22:10 +0300

    Initial revision.

---
+++ Local-Search-Brute-Force-and-Recursion-for-Selection-Operator/Rastrigin - Brute Force/visualize.r
@@ -0,0 +1,21 @@
+library(plot3D)
+
+y <- c(2, 3, 4, 5, 6, 7, 8, 9, 10, 11)
+x <- c(2, 3, 4, 5, 6, 7)
+
+name <- "Rastrigin - Brute Force"
+
+z <- as.matrix(read.csv("times.csv", sep=",", header=FALSE))
+rownames(z) <- x
+colnames(z) <- y
+persp3D(x,y,z, xlab="Recursion Depth", ylab="Population Size", zlab="Time [ms]", theta=-35, phi=35, clab=name)
+
+z <- as.matrix(read.csv("values.csv", sep=",", header=FALSE))
+rownames(z) <- x
+colnames(z) <- y
+persp3D(x,y,z, xlab="Recursion Depth", ylab="Population Size", zlab="Suboptimal Value", theta=110, phi=35, clab=name)
+
+z <- as.matrix(read.csv("evaluations.csv", sep=",", header=FALSE))
+rownames(z) <- x
+colnames(z) <- y
+persp3D(x,y,z, xlab="Recursion Depth", ylab="Population Size", zlab="Number of Evaluations", theta=-35, phi=35, clab=name)
